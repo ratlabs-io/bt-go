258743f4fbc8de7dd2ff9821c9878318ebdbaf29
Jason Fitzpatrick <jason@loop.baby>
2023-04-23 21:57:31 +0300

    remove interval from tree runner

--- runner.go
+++ runner.go
@@ -1,46 +1,24 @@
 package bt
 
-import (
-	"time"
-)
-
 // TreeRunner runs a behavior tree with a specified tick rate.
 type TreeRunner struct {
-	tree     Behavior
-	interval time.Duration
+	tree Behavior
 }
 
 // NewTreeRunner returns a new TreeRunner that runs the given behavior tree with the specified tick rate.
-func NewTreeRunner(tree Behavior, tickRate time.Duration) *TreeRunner {
+func NewTreeRunner(tree Behavior) *TreeRunner {
 	return &TreeRunner{
-		tree:     tree,
-		interval: tickRate,
+		tree: tree,
 	}
 }
 
 // Run runs the behavior tree until the context is done.
-// Run runs the behavior tree with the given context, executing the tree at the specified interval.
-// If the interval is 0, the tree is run continuously without waiting.
 func (tr *TreeRunner) Run(ctx *BehaviorContext) {
-	if tr.interval == 0 {
-		for {
-			select {
-			case <-ctx.Ctx.Done():
-				return
-			default:
-				tr.tree.Tick(ctx)
-			}
-		}
-	}
-
-	ticker := time.NewTicker(tr.interval)
-	defer ticker.Stop()
-
 	for {
 		select {
 		case <-ctx.Ctx.Done():
 			return
-		case <-ticker.C:
+		default:
 			tr.tree.Tick(ctx)
 		}
 	}
