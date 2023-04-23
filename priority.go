fa9b254f02df729f7afa62c4b29448d52507e31a
Jason Fitzpatrick <jason@loop.baby>
2023-04-23 18:49:40 +0300

    code cleanup

--- priority.go
+++ priority.go
@@ -3,29 +3,36 @@ package bt
 // PrioritySelector represents a behavior tree node that selects the first child that succeeds, and returns Failure if none succeed.
 type PrioritySelector struct {
 	Composite
-	runningChildIndex int
+	currentRunningIndex int
 }
 
 // NewPrioritySelector returns a new PrioritySelector with the given children.
 func NewPrioritySelector(children ...Behavior) *PrioritySelector {
 	return &PrioritySelector{
-		Composite:         Composite{Children: children},
-		runningChildIndex: -1,
+		Composite:           Composite{Children: children},
+		currentRunningIndex: -1,
 	}
 }
 
 // Tick iterates over the child nodes with the given BehaviorContext and returns the first Success status encountered.
 func (s *PrioritySelector) Tick(ctx *BehaviorContext) RunStatus {
-	for i, child := range s.Children {
+	// Iterate over child nodes
+	for i := 0; i < len(s.Children); i++ {
+		child := s.Children[i]
+		// Call Tick method of child node and handle return value
+		if child == nil {
+			return Failure // handle nil child node
+		}
 		status := child.Tick(ctx)
 		if status != Failure {
+			s.currentRunningIndex = -1
 			if status == Running {
-				s.runningChildIndex = i
-			} else {
-				s.runningChildIndex = -1
+				s.currentRunningIndex = i
 			}
 			return status
 		}
 	}
+	// No child node succeeded or is running
+	s.currentRunningIndex = -1
 	return Failure
 }
--- selector.go
+++ selector.go
@@ -3,29 +3,35 @@ package bt
 // Selector represents a behavior tree node that selects the first child that succeeds or is running.
 type Selector struct {
 	Composite
-	runningChildIndex int
+	currentRunningIndex int
 }
 
 // NewSelector returns a new Selector with the given children.
 func NewSelector(children ...Behavior) *Selector {
 	return &Selector{
-		Composite:         Composite{Children: children},
-		runningChildIndex: -1,
+		Composite:           Composite{Children: children},
+		currentRunningIndex: -1,
 	}
 }
 
 // Tick iterates over the child nodes with the given BehaviorContext and returns the first non-Failure status encountered.
 func (s *Selector) Tick(ctx *BehaviorContext) RunStatus {
+	// Iterate over child nodes
 	for i, child := range s.Children {
-		switch status := child.Tick(ctx); status {
-		case Success:
-			s.runningChildIndex = -1
+		// Call Tick method of child node and handle return value
+		if child == nil {
+			return Failure // handle nil child node
+		}
+		status := child.Tick(ctx)
+		if status == Success {
+			s.currentRunningIndex = -1
 			return Success
-		case Running:
-			s.runningChildIndex = i
+		} else if status == Running {
+			s.currentRunningIndex = i
 			return Running
 		}
 	}
-	s.runningChildIndex = -1
+	// No child node succeeded or is running
+	s.currentRunningIndex = -1
 	return Failure
 }
