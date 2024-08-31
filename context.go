ad93f206a049acb7150e7e2f67c99853fbbedca3
Jason Fitzpatrick <jason.g.fitzpatrick@gmail.com>
2024-09-01 01:08:18 +0200

    add types to context keys

--- context.go
+++ context.go
@@ -8,35 +8,35 @@ import (
 
 // BehaviorContext defines the interface for a synchronized context.
 type BehaviorContext interface {
-	Set(key, value interface{})
-	Get(key interface{}) (value interface{}, ok bool)
-	Delete(key interface{})
+	Set(key string, value interface{})
+	Get(key string) (value interface{}, ok bool)
+	Delete(key string)
 }
 
 // behaviorContextImpl is the concrete implementation of the BehaviorContext interface.
 type behaviorContextImpl struct {
 	Ctx         context.Context
 	mu          sync.RWMutex
-	ContextData map[interface{}]interface{}
+	ContextData map[string]interface{}
 }
 
 // NewBehaviorContext creates a new BehaviorContext with the specified base context.
 func NewBehaviorContext(ctx context.Context) BehaviorContext {
 	return &behaviorContextImpl{
 		Ctx:         ctx,
-		ContextData: make(map[interface{}]interface{}),
+		ContextData: make(map[string]interface{}),
 	}
 }
 
 // Set stores the value in the behaviorContextImpl for the given key.
-func (bc *behaviorContextImpl) Set(key, value interface{}) {
+func (bc *behaviorContextImpl) Set(key string, value interface{}) {
 	bc.mu.Lock()
 	defer bc.mu.Unlock()
 	bc.ContextData[key] = value
 }
 
 // Get retrieves the value from the behaviorContextImpl for the given key.
-func (bc *behaviorContextImpl) Get(key interface{}) (value interface{}, ok bool) {
+func (bc *behaviorContextImpl) Get(key string) (value interface{}, ok bool) {
 	bc.mu.RLock()
 	defer bc.mu.RUnlock()
 	value, ok = bc.ContextData[key]
@@ -44,7 +44,7 @@ func (bc *behaviorContextImpl) Get(key interface{}) (value interface{}, ok bool)
 }
 
 // Delete removes the value from the behaviorContextImpl for the given key.
-func (bc *behaviorContextImpl) Delete(key interface{}) {
+func (bc *behaviorContextImpl) Delete(key string) {
 	bc.mu.Lock()
 	defer bc.mu.Unlock()
 	delete(bc.ContextData, key)
